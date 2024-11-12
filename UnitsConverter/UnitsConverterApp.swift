f327575b5aa3fff3efb235d50317258b0b5b115c
Anna Filin <68apple@gmail.com>
2024-11-12 14:39:27 +0200

    Initial Commit

---
+++ UnitsConverter/UnitsConverterApp.swift
@@ -0,0 +1,17 @@
+//
+//  UnitsConverterApp.swift
+//  UnitsConverter
+//
+//  Created by Anna Filin on 12/11/2024.
+//
+
+import SwiftUI
+
+@main
+struct UnitsConverterApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
